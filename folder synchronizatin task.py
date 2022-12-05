93c7e7ad493bb687d67900d37e5ac5252cd6c2d1
Daniel <68239572+DanieloDanieli@users.noreply.github.com>
2022-12-05 18:46:26 +0100

    Update folder synchronizatin task.py

--- folder synchronizatin task.py
+++ folder synchronizatin task.py
@@ -2,11 +2,19 @@ import shutil
 import os
 import time
 import filecmp
+import sys
 
-def check_path_input(input_string):
+def log(message, filename):
+    with open(log_file, 'a') as f:
+                    f.write(message + ' ')
+                    f.write(filename)
+                    f.write('\n')
+    print(message, filename)
+
+def check_path_input(path):
     # checks if user path input is valid, otherwise raises an Exception
-    if os.path.exists(input_string):
-        return input_string
+    if os.path.exists(path):
+        return path
     else:
         raise Exception("Invalid path.")
 
@@ -22,19 +30,15 @@ def check_if_file_removed():
     for i in starting_source_folder:
         if i not in os.listdir(sf):
             starting_source_folder.remove(i)
-            print(i , 'was removed by user')
-            with open(log_file, 'a') as f:
-                    f.write(i)
-                    f.write(' was removed by user\n')
+            log_message = 'user removed'
+            log(log_message, i)
 
 def check_if_file_created():
     # checks if a file was created in current source folder by comparing it to its version at the start and logs it
     for i in os.listdir(sf):
         if i not in starting_source_folder:
-            print(i, 'was created by user')
-            with open(log_file, 'a') as f:
-                f.write(i)
-                f.write(' was created by user\n')
+            log_message = 'user created'
+            log(log_message, i)
             starting_source_folder.append(i)
         else: pass
 
@@ -44,18 +48,12 @@ def remove_redundant_files_dst(source_folder, destination_folder):
         if file_name not in os.listdir(source_folder):
             if os.path.isfile(os.path.join(destination_folder, file_name)):
                 os.remove(os.path.join(destination_folder, file_name))
-                print('removed', file_name)
-                with open(log_file, 'a') as f:
-                    f.write('removed ')
-                    f.write(file_name)
-                    f.write('\n')
+                log_message = 'removed'
+                log(log_message, file_name)
             else:
                 shutil.rmtree(os.path.join(destination_folder, file_name))
-                print('removed', file_name)
-                with open(log_file, 'a') as f:
-                    f.write('removed ')
-                    f.write(file_name)
-                    f.write('\n')
+                log_message = 'removed'
+                log(log_message, file_name)
         if os.path.isdir(os.path.join(destination_folder, file_name)):
             dst_folder = os.path.join(destination_folder, file_name)
             src_folder = os.path.join(source_folder, file_name)
@@ -72,39 +70,25 @@ def copy_files(source_folder, destination_folder):
 
         if os.path.isfile(source) and file_name not in os.listdir(destination_folder):
             shutil.copy(source, destination)
-            print('copied', file_name)
-            with open(log_file, 'a') as f:
-                f.write('copied ')
-                f.write(file_name)
-                f.write('\n')
+            log_message = 'copied'
+            log(log_message, file_name)
         if os.path.isdir(source):
             if file_name not in os.listdir(destination_folder):
                 os.makedirs(destination)
-                print('copied', file_name)
-                with open(log_file, 'a') as f:
-                    f.write('copied ')
-                    f.write(file_name)
-                    f.write('\n')
+                log_message = 'copied'
+                log(log_message,file_name)
             source_subfolder = source
             destination_subfolder = destination
             copy_files(source_subfolder, destination_subfolder)
 
 
-input_sf = input("Enter source folder path: ")
+sf = check_path_input(sys.argv[1])
 
-sf = check_path_input(input_sf)
+df = check_path_input(sys.argv[2])
 
-input_df = input("Enter replica folder path: ")
+log_file = check_path_input(sys.argv[3])
 
-df = check_path_input(input_df)
-
-input_logfile = input("Enter log file path: ")
-
-log_file = check_path_input(input_logfile)
-
-input_synchronization_interval = input("Enter synchronization interval in seconds: ")
-
-synchronization_interval = int(check_if_synchronization_interval_is_number(input_synchronization_interval))
+synchronization_interval = check_if_synchronization_interval_is_number(sys.argv[4])
 
 starting_source_folder = os.listdir(sf)
 
@@ -120,7 +104,6 @@ try:
 
         copy_files(sf, df)
 
-        time.sleep(synchronization_interval)
-
+        time.sleep(int(synchronization_interval))
 except:
     print("Error: Can't open this type of file while programme is running.")
